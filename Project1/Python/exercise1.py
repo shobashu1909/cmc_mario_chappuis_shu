dd92e1f21ec808a676c42c3a40ab0ae0976c1dd6
shobashu <shujiro1909@gmail.com>
2024-04-18 16:28:14 +0200

    0 to 2

--- Project1/Python/exercise1.py
+++ Project1/Python/exercise1.py
@@ -42,18 +42,18 @@ def exercise1():
             log_path = log_path,
             video_record = False,
             compute_metrics = 2,
-            amplitude = amp[i],
-            wave_frequency = wavefrequency[j],
+            amplitude = amp,
+            wave_frequency = wavefrequency,
             headless = True,
             print_metrics = True,
             return_network = True
         )
-        for i, amp in enumerate(np.linspace(0.05, 0.3, nsim))
-        for j, wavefrequency in enumerate(np.linspace(0., 0.1, nsim))
+        for i, amp in enumerate(np.linspace(0.05, 2, nsim))
+        for j, wavefrequency in enumerate(np.linspace(0., 2, nsim))
     ]
 
-    # pylog.info("Running the simulation")
-    # controllers = run_multiple(pars_list, num_process=16)
+    pylog.info("Running the simulation")
+    controllers = run_multiple(pars_list, num_process=16)
 
     fspeeds = np.zeros([nsim*nsim, 3])
     lspeeds = np.zeros([nsim*nsim, 3])
